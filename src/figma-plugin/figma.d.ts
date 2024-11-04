efd77f011ac6565c4451b18c7a1697c1f2bd72f9
Saulo Vallory <me@saulo.engineer>
2024-11-04 15:55:26 -0300

    feat: update figma typings
    Signed-off-by: Saulo Vallory <me@saulo.engineer>

--- src/figma-plugin/figma.d.ts
+++ src/figma-plugin/figma.d.ts
@@ -1,5 +1,7 @@
-/// <reference path="./plugin-api.d.ts" />
-declare global {
+/**
+ * @figma/plugin-typings@1.100.2
+ */
+
 /* plugin-typings are auto-generated. Do not update them directly. See plugin-docs/ for instructions. */
 declare type ArgFreeEventType =
   | 'selectionchange'
@@ -2600,60 +2602,7 @@ interface FindAllCriteria<T extends NodeType[]> {
     keys?: string[]
   }
 }
-}
-declare global {
-  // Global variable with Figma's plugin API.
-  const figma: PluginAPI
-  const __html__: string
-  const __uiFiles__: {
-    [key: string]: string
-  }
-  const console: Console
 
-  // The plugin environment exposes the browser console API,
-  // so expected calls like console.log() still work.
-  interface Console {
-    log(message?: any, ...optionalParams: any[]): void
-    error(message?: any, ...optionalParams: any[]): void
-    assert(condition?: boolean, message?: string, ...data: any[]): void
-    info(message?: any, ...optionalParams: any[]): void
-    warn(message?: any, ...optionalParams: any[]): void
-    clear(): void
-  }
-  function setTimeout(callback: Function, timeout: number): number
-  function clearTimeout(handle: number): void
-  function setInterval(callback: Function, timeout: number): number
-  function clearInterval(handle: number): void
-
-  const fetch: (url: string, init?: FetchOptions) => Promise<FetchResponse>
-
-  interface FetchOptions {
-    method?: string
-    headers?: { [name: string]: string }
-    /**
-     * @deprecated use headers instead
-     */
-    headersObject?: { [name: string]: string }
-    body?: Uint8Array | string
-    credentials?: string
-    cache?: string
-    redirect?: string
-    referrer?: string
-    integrity?: string
-  }
-
-  interface FetchResponse {
-    headersObject: { [name: string]: string }
-    ok: boolean
-    redirected: boolean
-    status: number
-    statusText: string
-    type: string
-    url: string
-    arrayBuffer(): Promise<ArrayBuffer>
-    text(): Promise<string>
-    json(): Promise<any>
-  }
-} // declare global
 
 export {}
+
